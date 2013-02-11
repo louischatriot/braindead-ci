ff828c1aa70acfb2f9fa59048ca85459c94e34a9
Louis Chatriot <louis.chatriot@gmail.com>
2013-02-11 13:40:56 +0100

    Able to save numbers in the project conf files

--- routes/index.js
+++ routes/index.js
@@ -26,13 +26,7 @@ Job.getJob(testJob, function (err, job) {
     //job.reinstallDependencies(function () {
       //console.log("REINSTALLED");
 
-      job.runTests(function (testStdout) {
-        testStdout.on('data', function (data) {
-          res.write(data.toString('utf8'));
-        });
-        //testStdout.pipe(res);
-      }, function (err) {
-        //res.end(204);
+      job.runTests([res, process.stdout], function (err) {
         console.log("TEST RESULT");
         console.log(err);
       });
