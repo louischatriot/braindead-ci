a3d25ce47f2915ab5f7ed1b7cbb7cdd4ad9608e4
Louis Chatriot <louis.chatriot@gmail.com>
2013-02-11 08:55:05 +0100

    Somehow able to pipe tests results to http response

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@
 
 
 var config = require('../lib/config')
+  , Job = require('../lib/job')
   ;
 
 module.exports = function (req, res, next) {
@@ -11,8 +12,37 @@ module.exports = function (req, res, next) {
     , partials = { content: '{{>pages/index}}' }
     ;
 
-  return res.render('layout', { values: values
-                              , partials: partials
-                              });
+var testJob = { name: 'api'
+              , repo: 'git@github.com:tldrio/tldr-api.git'
+              , branch: 'master'
+              };
+
+
+Job.getJob(testJob, function (err, job) {
+  //job.pullRepo(function (err) {
+    //console.log("REPO PULLED");
+    //console.log(err);
+
+    //job.reinstallDependencies(function () {
+      //console.log("REINSTALLED");
+
+      job.runTests(function (testStdout) {
+        testStdout.on('data', function (data) {
+          res.write(data.toString('utf8'));
+        });
+        //testStdout.pipe(res);
+      }, function (err) {
+        //res.end(204);
+        console.log("TEST RESULT");
+        console.log(err);
+      });
+    //});
+  //});
+});
+
+
+  //return res.render('layout', { values: values
+                              //, partials: partials
+                              //});
 
 };
