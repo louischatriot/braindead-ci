19b3ab10a617ec1588106ca975ddd0a2f78b88a6
Louis Chatriot <louis.chatriot@gmail.com>
2013-05-07 16:27:34 +0200

    Good authentication

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,13 @@
-var db = require('../lib/db');
+var db = require('../lib/db')
+  , User = require('../lib/user')
+  , _ = require('underscore')
+  ;
 
 function displayForm (req, res, next) {
   var values = req.renderValues || {}
     , partials = { content: '{{>pages/login}}' }
 
   values.userInput = values.userInput || {};
-  //req.session.userId = "RJ4V6xZDDphMwcZQ";
 
   return res.render('layout', { values: values
                               , partials: partials
@@ -14,11 +16,26 @@ function displayForm (req, res, next) {
 
 
 function checkCredentials (req, res, next) {
-  if (! req.body.login) { return res.send(401); }
+  req.renderValues.userInput = { login: req.body.login, password: req.body.password };
 
-  db.users.findOne({ login: req.body.login }, function (err, user) {
-    if (err || !user || user.password !== req.body.password) {
-      req.renderValues.userInput = { login: req.body.login, password: req.body.password };
+  if (!req.body.login) {
+    req.renderValues.validationErrors = true;
+    req.renderValues.errors = ['Please enter a login'];
+    return displayForm(req, res, next);
+  }
+
+  if (!req.body.password) {
+    req.renderValues.validationErrors = true;
+    req.renderValues.errors = ['Please enter your password'];
+    return displayForm(req, res, next);
+  }
+
+  User.checkCredentials(req.body.login, req.body.password, function (err, user) {
+    if (err) {
+      if (err.validationErrors) {
+        req.renderValues.validationErrors = true;
+        req.renderValues.errors = _.values(err.validationErrors);
+      }
       return displayForm(req, res, next);
     }
 
