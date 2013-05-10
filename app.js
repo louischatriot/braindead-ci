5067bb84fe14c65b2f22a41bd1a8d340a776ca12
Louis Chatriot <louis.chatriot@gmail.com>
2013-05-10 14:53:02 +0200

    Two job types: basic and node

--- app.js
+++ app.js
@@ -2,10 +2,56 @@ var Job = require('./lib/job')
   , config = require('./lib/config')
   , customUtils = require('./lib/customUtils')
   , async = require('async')
-  , jobsMetadata = {}
+  , fs = require('fs')
+  , path = require('path')
   , server = require('./server')
   , db = require('./lib/db')
+  , jobTypes = {}
   ;
+
+
+/**
+ * Initialize the list of job types
+ * For now no plugin system is implemented, we only look at modules in the ./lib/jobTypes directory
+ * @param {Function} callback Signature: err
+ */
+function initializeJobTypes (callback) {
+  var nativeJobsTypesDirectory = './lib/jobTypes';
+
+  fs.readdir(nativeJobsTypesDirectory, function (err, files) {
+    if (err) { return callback(err); }
+
+    async.each(files, function (file, cb) {
+      var moduleName = path.join(nativeJobsTypesDirectory, file).replace(/\.js$/, '')
+        , jobType
+        ;
+
+      if (!moduleName.match(/^\.\//)) {
+        moduleName = './' + moduleName;
+      }
+
+      try {
+        jobType = require(moduleName);
+      } catch (e) {
+        jobType = {};
+      }
+
+      if (jobType.populateBuildingSequence) {
+        jobTypes[jobType.name] = jobType.populateBuildingSequence;
+      }
+
+      return cb();
+    }, callback);
+  });
+}
+
+
+/**
+ * Get all job types
+ */
+function getAllJobTypes () {
+  return jobTypes;
+}
 
 
 /**
@@ -39,3 +85,7 @@ if (module.parent === null) {
     }
   });
 }
+
+
+// Interface
+module.exports.getAllJobTypes = getAllJobTypes;
