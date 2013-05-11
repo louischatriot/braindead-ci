ebe19da95ba4b024e978a57b956b8b2da00f2715
Louis Chatriot <louis.chatriot@gmail.com>
2013-05-11 13:00:28 +0200

    User creation tested

--- test/user.test.js
+++ test/user.test.js
@@ -6,11 +6,12 @@ var should = require('chai').should()
 
 describe('User', function () {
 
+  before(function (done) {
+    db.initialize(done);
+  });
+
   beforeEach(function (done) {
-    db.initialize(function (err) {
-      if (err) { return done(err); }
-      db.users.remove({}, function (err) { return done(err); });
-    });
+    db.users.remove({}, function (err) { return done(err); });
   });
 
   it('Can check whether the user db is empty', function (done) {
@@ -25,6 +26,50 @@ describe('User', function () {
 
             done();
           });
+        });
+      });
+    });
+  });
+
+  it('Can create a user and encrypt his password', function (done) {
+    var userData = { login: 'test', password: 'supersecret' };
+
+    User.userDbEmpty(function (empty) {
+      empty.should.equal(true);
+
+      User.createUser(userData, function (err, newUser) {
+        newUser.login.should.equal('test');
+        assert.isDefined(newUser.password.salt);
+        newUser.password.derivedKey.should.not.equal('supersecret');
+
+        done();
+      });
+    });
+  });
+
+  it('Wont validate if login or password is not present or too short', function () {
+    assert.isDefined(User.validate({ login: 'coolLogin' }).validationErrors.password);
+    assert.isDefined(User.validate({ password: 'coolPassword' }).validationErrors.login);
+    assert.isDefined(User.validate({ login: 'co', password: 'coolPassword' }).validationErrors.login);
+    assert.isDefined(User.validate({ login: 'coolLogin', password: 'short' }).validationErrors.password);
+
+    assert.isNull(User.validate({ login: 'coolLogin', password: 'coolPassword' }));
+  });
+
+  it('Wont create a user if it doesnt validate or login is already taken', function (done) {
+    var userData = { login: 'again', password: 'short' };
+
+    User.createUser(userData, function (err) {
+      assert.isDefined(err.validationErrors.password);
+
+      userData.password = 'supersecret';
+      User.createUser(userData, function (err) {
+        assert.isNull(err);
+
+        User.createUser(userData, function (err) {
+          assert.isDefined(err.validationErrors.login);
+
+          done();
         });
       });
     });
