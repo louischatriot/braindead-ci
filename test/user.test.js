d6528a67e5a2d824c050de3ce84712f7d045a60b
Louis Chatriot <louis.chatriot@gmail.com>
2013-05-11 12:28:14 +0200

    Changed config so that we can test with all command line args

---
+++ test/user.test.js
@@ -0,0 +1,14 @@
+var should = require('chai').should()
+  , assert = require('chai').assert
+  , db = require('../lib/db')
+  , User = require('../lib/user')
+  ;
+
+describe('User', function () {
+
+  it('Dummy', function () {
+
+  });
+
+
+});
