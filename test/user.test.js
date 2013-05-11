8c862763eb9852c55341a13af4763d9c050424ba
Louis Chatriot <louis.chatriot@gmail.com>
2013-05-11 12:34:48 +0200

    Can test whether db is empty

--- test/user.test.js
+++ test/user.test.js
@@ -6,9 +6,28 @@ var should = require('chai').should()
 
 describe('User', function () {
 
-  it('Dummy', function () {
-
+  beforeEach(function (done) {
+    db.initialize(function (err) {
+      if (err) { return done(err); }
+      db.users.remove({}, function (err) { return done(err); });
+    });
   });
 
+  it('Can check whether the user db is empty', function (done) {
+    User.getAllUsers(function (err, users) {
+      users.length.should.equal(0);
+      User.userDbEmpty(function (empty) {
+        empty.should.equal(true);
+
+        User.createUser({ login: 'test', password: 'secretpassword' }, function () {
+          User.userDbEmpty(function (empty) {
+            empty.should.equal(false);
+
+            done();
+          });
+        });
+      });
+    });
+  });
 
 });
